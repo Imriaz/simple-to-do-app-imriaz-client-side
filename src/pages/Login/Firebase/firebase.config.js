09cf01f3f427051fc2524f40c6e4b8b8481c253a
Md. Imriaz Uddin <imriaz.cse.bubt@gmail.com>
2021-12-03 14:50:23 +0600

    To Do list app done

--- src/pages/Login/Firebase/firebase.config.js
+++ src/pages/Login/Firebase/firebase.config.js
@@ -1,19 +1,10 @@
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_APP_ID,
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyA8euCcmB6G9M2IelJTxjOaHM1KzIXfBE4",
-    authDomain: "to-do-app-by-imriaz.firebaseapp.com",
-    projectId: "to-do-app-by-imriaz",
-    storageBucket: "to-do-app-by-imriaz.appspot.com",
-    messagingSenderId: "827552322463",
-    appId: "1:827552322463:web:85d4daf03c670beccee3aa"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebaseConfig;
