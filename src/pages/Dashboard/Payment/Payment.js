534b6c460dce00909e46215d04e4ee46e557f244
Md. Imriaz Uddin <imriaz.cse.bubt@gmail.com>
2021-12-03 10:36:36 +0600

    Paypal Method done

--- src/pages/Dashboard/Payment/Payment.js
+++ src/pages/Dashboard/Payment/Payment.js
@@ -1,21 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import PayPal from './PayPal';
 
 const Payment = () => {
     const { mySubscriptionId } = useParams();
     const [mySubscription, setMySubscription] = useState({});
+    const [checkout, setCheckout] = useState(false);
+
     useEffect(() => {
         fetch(`http://localhost:5000/orders/${mySubscriptionId}`)
             .then(res => res.json())
             .then(data => setMySubscription(data));
     }, [mySubscriptionId])
+
+
     return (
         <div>
             <h1 className='text-info'>Please Pay: {mySubscription.price}$ for {mySubscription.packageName} Package</h1>
             <br />
             <h3>Total Pay: {mySubscription.price}</h3>
             <br />
-
+            {checkout ? (
+                <PayPal />
+            ) : (
+                <button onClick={() => {
+                    setCheckout(true);
+                }}>
+                    Checkout</button>
+            )
+            }
         </div>
     );
 };
